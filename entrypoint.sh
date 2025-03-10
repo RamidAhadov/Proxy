43b8a89733aa0beaf58790ff679c6e00ec196138
rahadov <ramid.ahadov@simbrella.com>
2025-03-10 19:50:27 +0400

    entrypoint changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 ﻿#!/bin/bash
 
 CONFIG_FILE="/config/appsettings.json"
-DEFAULT_CONFIG="/etc/Proxy/Configuration/appsettings.json"
+ENV_FILE="/config/env.sh"
+
+if [ -f "$ENV_FILE" ]; then
+    source "$ENV_FILE"
+fi
 
 if [ ! -f "$CONFIG_FILE" ]; then
-    cp "$DEFAULT_CONFIG" "$CONFIG_FILE"
+    echo "Error: Config file $CONFIG_FILE not found!"
+    exit 1
 fi
 
 jq --arg ip "$BOOTSTRAP_SERVER" '.KafkaSettings.BootstrapServers = $ip' "$CONFIG_FILE" > temp.json && mv temp.json "$CONFIG_FILE"
