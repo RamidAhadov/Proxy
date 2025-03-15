e4076337a220355f72cf0a7f53bacc034d68b3fc
rahadov <ramid.ahadov@simbrella.com>
2025-03-16 01:51:55 +0400

    Back to HTTP

--- Proxy.DataService/Program.cs
+++ Proxy.DataService/Program.cs
@@ -2,70 +2,53 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Http;
 using Proxy.DataService.Configuration.ConfigItems;
 using Proxy.DataService.Extensions;
-using System.Threading.Tasks;
 
-namespace Proxy.DataService;
-
-public class Program
+namespace Proxy.DataService
 {
-    public static async Task Main(string[] args)
+    public class Program
     {
-        string configPath = "/local/appsettings.json";
-
-        if (!File.Exists(configPath))
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine($"Error: Config file {configPath} not found!");
-            return;
-        }
+            string configPath = "/local/appsettings.json";
 
-        if (File.Exists("/local/wildcard.pfx"))
-        {
-            Console.WriteLine("✅ Wildcard PFX file found!");
-            Console.WriteLine($"File Size: {new FileInfo("/local/wildcard.pfx").Length} bytes");
-        }
-        else
-        {
-            Console.WriteLine("❌ ERROR: Wildcard PFX file NOT found!");
-        }
-
-        IConfigurationBuilder builder = new ConfigurationBuilder()
-            .SetBasePath(Path.GetDirectoryName(configPath)!)
-            .AddJsonFile(configPath, optional: false, reloadOnChange: true);
-
-        IConfigurationRoot root = builder.Build();
-
-        var builderWeb = WebApplication.CreateBuilder(args);
-        ControllerSettings controllerSettings = new();
-        root.GetSection("ControllerSettings").Bind(controllerSettings);
-        
-        builderWeb.WebHost.ConfigureKestrel(options =>
-        {
-            options.ListenAnyIP(controllerSettings.Port, listenOptions =>
+            if (!File.Exists(configPath))
             {
-                listenOptions.UseHttps(controllerSettings.CertPath, controllerSettings.CertPassword);
+                Console.WriteLine($"Error: Config file {configPath} not found!");
+                return;
+            }
+
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Path.GetDirectoryName(configPath)!)
+                .AddJsonFile(configPath, optional: false, reloadOnChange: true);
+
+            IConfigurationRoot root = builder.Build();
+
+            var builderWeb = WebApplication.CreateBuilder(args);
+            ControllerSettings controllerSettings = new();
+            root.GetSection("ControllerSettings").Bind(controllerSettings);
+
+            builderWeb.WebHost.UseUrls($"http://0.0.0.0:{controllerSettings.Port}");
+
+            builderWeb.Services
+                .ConfigureSettings(root)
+                .ConfigureServices()
+                .ConfigureLogging()
+                .AddControllers();
+
+            var app = builderWeb.Build();
+
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine($"[GLOBAL LOG] Incoming request: {context.Request.Method} {context.Request.Path} {context.Request.QueryString}");
+                await next.Invoke();
             });
-        });
 
-        builderWeb.Services
-            .ConfigureSettings(root)
-            .ConfigureServices()
-            .ConfigureLogging()
-            .AddControllers();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
 
-        var app = builderWeb.Build();
-
-        app.Use(async (context, next) =>
-        {
-            Console.WriteLine($"[GLOBAL LOG] Incoming request: {context.Request.Method} {context.Request.Path} {context.Request.QueryString}");
-            await next.Invoke();
-        });
-
-        app.UseRouting();
-        app.UseEndpoints(endpoints => endpoints.MapControllers());
-
-        await app.RunAsync();
+            await app.RunAsync();
+        }
     }
 }
