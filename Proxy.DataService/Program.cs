8c3b7e2bd91728e6de562bf071c0ddeb98bee018
rahadov <ramid.ahadov@simbrella.com>
2025-03-16 03:06:45 +0400

    Back to HTTPS

--- Proxy.DataService/Program.cs
+++ Proxy.DataService/Program.cs
@@ -2,53 +2,60 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
 using Proxy.DataService.Configuration.ConfigItems;
 using Proxy.DataService.Extensions;
+using System.Threading.Tasks;
 
-namespace Proxy.DataService
+namespace Proxy.DataService;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
+        string configPath = "/local/appsettings.json";
+
+        if (!File.Exists(configPath))
         {
-            string configPath = "/local/appsettings.json";
-
-            if (!File.Exists(configPath))
-            {
-                Console.WriteLine($"Error: Config file {configPath} not found!");
-                return;
-            }
-
-            IConfigurationBuilder builder = new ConfigurationBuilder()
-                .SetBasePath(Path.GetDirectoryName(configPath)!)
-                .AddJsonFile(configPath, optional: false, reloadOnChange: true);
-
-            IConfigurationRoot root = builder.Build();
-
-            var builderWeb = WebApplication.CreateBuilder(args);
-            ControllerSettings controllerSettings = new();
-            root.GetSection("ControllerSettings").Bind(controllerSettings);
-
-            builderWeb.WebHost.UseUrls($"http://0.0.0.0:{controllerSettings.Port}");
-
-            builderWeb.Services
-                .ConfigureSettings(root)
-                .ConfigureServices()
-                .ConfigureLogging()
-                .AddControllers();
-
-            var app = builderWeb.Build();
-
-            app.Use(async (context, next) =>
-            {
-                Console.WriteLine($"[GLOBAL LOG] Incoming request: {context.Request.Method} {context.Request.Path} {context.Request.QueryString}");
-                await next.Invoke();
-            });
-
-            app.UseRouting();
-            app.UseEndpoints(endpoints => endpoints.MapControllers());
-
-            await app.RunAsync();
+            Console.WriteLine($"Error: Config file {configPath} not found!");
+            return;
         }
+
+        IConfigurationBuilder builder = new ConfigurationBuilder()
+            .SetBasePath(Path.GetDirectoryName(configPath)!)
+            .AddJsonFile(configPath, optional: false, reloadOnChange: true);
+
+        IConfigurationRoot root = builder.Build();
+
+        var builderWeb = WebApplication.CreateBuilder(args);
+        ControllerSettings controllerSettings = new();
+        root.GetSection("ControllerSettings").Bind(controllerSettings);
+        
+        builderWeb.WebHost.ConfigureKestrel(options =>
+        {
+            options.ListenAnyIP(controllerSettings.Port, listenOptions =>
+            {
+                listenOptions.UseHttps(controllerSettings.CertPath, controllerSettings.CertPassword);
+            });
+        });
+
+        builderWeb.Services
+            .ConfigureSettings(root)
+            .ConfigureServices()
+            .ConfigureLogging()
+            .AddControllers();
+
+        var app = builderWeb.Build();
+
+        app.Use(async (context, next) =>
+        {
+            Console.WriteLine($"[GLOBAL LOG] Incoming request: {context.Request.Method} {context.Request.Path} {context.Request.QueryString}");
+            await next.Invoke();
+        });
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+        await app.RunAsync();
     }
 }
