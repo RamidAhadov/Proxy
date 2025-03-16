e4cde71fbef2201251debbff2a010035a0573200
rahadov <ramid.ahadov@simbrella.com>
2025-03-16 22:19:02 +0400

    Prometheus and metric reporters

---
+++ Proxy.DataService/Monitoring/PrometheusMetricReporter.cs
@@ -0,0 +1,33 @@
+﻿using Prometheus;
+using Proxy.DataService.Monitoring.Abstractions;
+
+namespace Proxy.DataService.Monitoring;
+
+public class PrometheusMetricReporter : IServiceMetricReporter
+{
+    public PrometheusMetricReporter()
+    {
+        // In order to not see no data in startup
+        _receivedMessagesTotal.IncTo(0);
+    }
+    
+    private readonly Counter _receivedMessagesTotal = Metrics.CreateCounter(
+        "received_messages",
+        "Total received messages by topic",
+        labelNames: ["topic"]);
+    
+    private readonly Counter _receivedMessageErrorsTotal = Metrics.CreateCounter(
+        "received_message_errors",
+        "Total errors for received messages by topic",
+        labelNames: ["topic"]);
+    
+    public void RegisterReceivedMessage(string topic)
+    {
+        _receivedMessagesTotal.WithLabels(topic).Inc();
+    }
+
+    public void RegisterReceivedMessageError(string topic)
+    {
+        _receivedMessageErrorsTotal.WithLabels(topic).Inc();
+    }
+}
