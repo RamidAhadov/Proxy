e87c4ca43ffa39b1885667833c13cf0d8bb861c2
rahadov <ramid.ahadov@simbrella.com>
2025-03-17 17:00:58 +0400

    Publish at startup removed

--- Proxy.DataService/Monitoring/PrometheusMetricReporter.cs
+++ Proxy.DataService/Monitoring/PrometheusMetricReporter.cs
@@ -5,12 +5,6 @@ namespace Proxy.DataService.Monitoring;
 
 public class PrometheusMetricReporter : IServiceMetricReporter
 {
-    public PrometheusMetricReporter()
-    {
-        // In order to not see no data in startup
-        _receivedMessagesTotal.IncTo(0);
-    }
-    
     private readonly Counter _receivedMessagesTotal = Metrics.CreateCounter(
         "received_messages",
         "Total received messages by topic",
